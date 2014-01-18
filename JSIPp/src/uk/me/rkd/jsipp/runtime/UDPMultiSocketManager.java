68631d7e5a337b1fe2a9f893d53b4a02752e87af
Rob Day <rkd@rkd.me.uk>
2014-01-18 16:20:12 +0000

    Handle all selector work on the same thread to avoid locking problems.

--- JSIPp/src/uk/me/rkd/jsipp/runtime/UDPMultiSocketManager.java
+++ JSIPp/src/uk/me/rkd/jsipp/runtime/UDPMultiSocketManager.java
@@ -5,6 +5,7 @@ import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
+import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
@@ -19,11 +20,22 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class UDPMultiSocketManager extends SocketManager {
 
-	private Selector selector;
-	private Map<Integer, SelectionKey> callNumToSocket;
+	Selector selector;
+	Map<Integer, SelectionKey> callNumToSocket;
 	private SocketAddress defaultTarget;
 	private Thread readerThread;
-	private Queue<SelectionKey> deadKeyQueue;
+	Queue<SelectionKey> deadKeyQueue;
+	Queue<CallAndChan> newCallQueue;
+	
+	private class CallAndChan {
+		public Call call;
+		public DatagramChannel chan;
+		
+		public CallAndChan(Call call, DatagramChannel chan) {
+			this.call = call;
+			this.chan = chan;
+		}
+	}
 	
 	public UDPMultiSocketManager(String defaultHost, int defaultPort) throws IOException {
 		this.defaultTarget = new InetSocketAddress(defaultHost, defaultPort);
@@ -32,6 +44,7 @@ public class UDPMultiSocketManager extends SocketManager {
 		this.readerThread = new SelectorThread();
 		this.readerThread.start();
 		this.deadKeyQueue = new ConcurrentLinkedQueue<SelectionKey>();
+		this.newCallQueue = new ConcurrentLinkedQueue<CallAndChan>();
 		// TODO Auto-generated constructor stub
 	}
 	
@@ -44,10 +57,9 @@ public class UDPMultiSocketManager extends SocketManager {
 	private class SelectorThread extends Thread {
 		public void run() {
 			int available = 0;
-			int max = 400;
 			while (!this.isInterrupted() && UDPMultiSocketManager.this.selector.isOpen()) {
 				try {
-					available = UDPMultiSocketManager.this.selector.select(50);
+					available = UDPMultiSocketManager.this.selector.select();
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
 					System.out.println("Selector is closed, terminating thread");
@@ -73,26 +85,23 @@ public class UDPMultiSocketManager extends SocketManager {
 						}
 						keyIterator.remove();
 					}
-				} else {
-					// Premature wakeup to allow a registration - don't call select again immediately
-					try {
-						Thread.sleep(0, 100);
-					} catch (InterruptedException e) {
-						// We don't really care
-					}
 				}
 				
 				while (!UDPMultiSocketManager.this.deadKeyQueue.isEmpty()) {
 					SelectionKey key = UDPMultiSocketManager.this.deadKeyQueue.poll();
-					DatagramChannel chan = (DatagramChannel)key.channel();
-					try {
-						chan.close();
-						System.out.println("Channel closed");
-					} catch (IOException e) {
-						System.out.println("IOException");
-						// TODO Auto-generated catch block
-					}	
 					key.cancel();
+				}
+				
+				while (!UDPMultiSocketManager.this.newCallQueue.isEmpty()) {
+					CallAndChan cnas = UDPMultiSocketManager.this.newCallQueue.poll();					
+					SelectionKey key;
+					try {
+						key = cnas.chan.register(UDPMultiSocketManager.this.selector, (SelectionKey.OP_READ), cnas.call);
+						UDPMultiSocketManager.this.callNumToSocket.put(new Integer(cnas.call.getNumber()), key);
+					} catch (ClosedChannelException e) {
+						// Nothing to worry about - if the channel is closed, we won't create the key, so no cleanup is needed,
+						// and we won't ever need to handle any calls coming in from it.
+					}
 				}
 			}
 		}
@@ -116,25 +125,17 @@ public class UDPMultiSocketManager extends SocketManager {
 
 	@Override
 	public void add(Call call) throws IOException {
-		long start = System.currentTimeMillis();
 		DatagramChannel chan = DatagramChannel.open();
-		//System.out.println("Socket opened " + Long.toString(System.currentTimeMillis() - start));
 		chan.connect(this.defaultTarget);
-		//System.out.println("Socket connected " + Long.toString(System.currentTimeMillis() - start));
 		chan.configureBlocking(false);
+		this.newCallQueue.add(new CallAndChan(call, chan));
 		selector.wakeup();
-		SelectionKey key = chan.register(selector, (SelectionKey.OP_READ), call);
-		//System.out.println("Socket registered " + Long.toString(System.currentTimeMillis() - start));
-		this.callNumToSocket.put(call.getNumber(), key);
-		// TODO Auto-generated method stub
-
 	}
 	
 	@Override
 	public void remove(Call call) throws IOException {
 		SelectionKey key = this.callNumToSocket.get(call.getNumber());
 		this.callNumToSocket.remove(call.getNumber());
-		//this.deadKeyQueue.add(key);
 		DatagramChannel chan = (DatagramChannel)key.channel();
 		try {
 			chan.close();
@@ -143,7 +144,8 @@ public class UDPMultiSocketManager extends SocketManager {
 			System.out.println("IOException");
 			// TODO Auto-generated catch block
 		}	
-		key.cancel();
+		this.deadKeyQueue.add(key);
+		selector.wakeup();
 		System.out.println("Socket removed");
 	}
 
